bcf2987b21f41f919985d173775a18a3194d1e01
Carlos Falgueras García <fark.zano@gmail.com>
2013-01-11 20:53:01 +0100

    Creado método "toString" para la clase "Tipo".

--- src/Tipo.java
+++ src/Tipo.java
@@ -1,4 +1,24 @@
-
 public enum Tipo {
 	NUM, TXT, TIM;
+	
+	public String toString(Tipo tipo){
+		String ret = "";
+
+		switch(tipo){
+			case NUM:
+				ret = "Número";
+			break;
+			
+			case TXT:
+				ret = "Texto";
+			break;
+			
+			case TIM:
+				ret = "Tiempo";
+			break;
+			
+		}
+
+		return ret;
+	}
 }
